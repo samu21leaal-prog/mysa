0b16cc326dfd865334b7c76421652eebfad908c4
samu21leaal-prog <samu21leaal@gmail.com>
2026-02-27 19:33:13 -0300

    Delete api directory

--- api/env-check.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    has_ML_CLIENT_ID: !!process.env.ML_CLIENT_ID,
-    has_ML_CLIENT_SECRET: !!process.env.ML_CLIENT_SECRET,
-    has_ML_REDIRECT_URI: !!process.env.ML_REDIRECT_URI,
-    vercel_env: process.env.VERCEL_ENV || null,
-  });
-}
